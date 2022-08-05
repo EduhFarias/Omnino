8549d1f6cd440ecec52cb20475d81b850e2c5aab
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2022-08-05 11:48:28 -0300

    Add files via upload

--- omnino_driver/omnino_driver/omnino_driver_node.py
+++ omnino_driver/omnino_driver/omnino_driver_node.py
@@ -6,83 +6,88 @@ import struct
 
 from geometry_msgs.msg import Twist
 
-MOTOR1 = b'\xf5'	# address serial motor 1
-MOTOR2 = b'\xfa'	# address serial motor 2
-MOTOR3 = b'\xff'	# address serial motor 3
+MOTOR1 = b'\xf5'  # address serial motor 1
+MOTOR2 = b'\xfa'  # address serial motor 2
+MOTOR3 = b'\xff'  # address serial motor 3
 
-FOWARD = b'\x66'	# turn motor foward
+FOWARD = b'\x66'  # turn motor foward
 BACKWARD = b'\x62' 	# turn motor backward
 STOP = b'\x73'	      	# stop motor
 
-def serial_motor(vel1,vel2,vel3):
-	serial.write(MOTOR1)
-	if vel1 < 0:
-		serial.write(struct.pack("B", vel1 * -1))
-	  serial.write(FOWARD)
-	if vel1> 0:
-		serial.write(struct.pack("B", vel1))
- 		serial.write(BACKWARD)
-	if vel1 == 0:	
-		serial.write(struct.pack("B", 0))
- 		serial.write(STOP)
 
-	serial.write(MOTOR2)
-	if vel2 < 0:
-		serial.write(struct.pack("B", vel2 * -1)) 
- 		serial.write(FOWARD)
-	if vel2 > 0:
-		serial.write(struct.pack("B", vel2 ))
- 		serial.write(BACKWARD)
- 	if vel2 == 0:
-		serial.write(struct.pack("B", 0))
- 		serial.write(STOP)	
+def serial_motor(vel1, vel2, vel3):
+    serial.write(MOTOR1)
+    if vel1 < 0:
+        serial.write(struct.pack("B", vel1 * -1))
+        serial.write(FOWARD)
+    if vel1 > 0:
+        serial.write(struct.pack("B", vel1))
+        serial.write(BACKWARD)
+    if vel1 == 0:
+        serial.write(struct.pack("B", 0))
+        serial.write(STOP)
 
-	serial.write(MOTOR3)
-	if vel3 < 0:
-		serial.write(struct.pack("B", vel3 * -1))
- 		serial.write(FOWARD)
-	if vel3 > 0:
-		serial.write(struct.pack("B", vel3))
- 		serial.write(BACKWARD)
-	if vel3 == 0:
-		serial.write(struct.pack("B", 0))
- 		serial.write(STOP)
+    serial.write(MOTOR2)
+    if vel2 < 0:
+        serial.write(struct.pack("B", vel2 * -1))
+        serial.write(FOWARD)
+    if vel2 > 0:
+        serial.write(struct.pack("B", vel2))
+        serial.write(BACKWARD)
+    if vel2 == 0:
+        serial.write(struct.pack("B", 0))
+        serial.write(STOP)
+
+    serial.write(MOTOR3)
+    if vel3 < 0:
+        serial.write(struct.pack("B", vel3 * -1))
+        serial.write(FOWARD)
+    if vel3 > 0:
+        serial.write(struct.pack("B", vel3))
+        serial.write(BACKWARD)
+    if vel3 == 0:
+        serial.write(struct.pack("B", 0))
+        serial.write(STOP)
+
 
 class OmninoDriverNode(Node):
-  def __init__(self):
-  	super().__init__('omnino_driver')
-		serial = serial.Serial('/dev/serial0', 9600)
-	
-		self.declare_parameter("wheel_r", 0.2)
-		self.declare_parameter("wheel_d", 1)
-		
-		self.r = self.get_parameter("wheel_r").get_parameter_value().double_value
-		self.d = self.get_parameter("wheel_d").get_parameter_value().double_value
+    def __init__(self):
+        super().__init__('omnino_driver')
+        serial = serial.Serial('/dev/serial0', 9600)
 
-    self.sub_ = self.create_subscription(Twist, 'cmd_vel', self.driver_callback, 10)
+        self.declare_parameter("wheel_r", 0.2)
+        self.declare_parameter("wheel_d", 1)
 
-	def driver_callback(self, cmd_vel):
-    vx = cmd_vel.linear.x
-    vy = cmd_vel.linear.y
-    w = cmd_vel.angular.z
-    
-    vel_1 = 1/self.r * (vx - self.d*w) 
-    vel_2 = 1/self.r * (-1/2*vx - sqrt(3)/2*vy - self.d*w)
-    vel_3 = 1/self.r * (-1/2*vx + sqrt(3)/2*vy - self.d*w)
-    
-    serial_motor(vel_1, vel_2, vel_3)
+        self.r = self.get_parameter(
+            "wheel_r").get_parameter_value().double_value
+        self.d = self.get_parameter(
+            "wheel_d").get_parameter_value().double_value
+
+        self.sub_ = self.create_subscription(
+            Twist, 'cmd_vel', self.driver_callback, 10)
+
+        def driver_callback(self, cmd_vel):
+            vx = cmd_vel.linear.x
+            vy = cmd_vel.linear.y
+            w = cmd_vel.angular.z
+
+            vel_1 = 1/self.r * (vx - self.d*w)
+            vel_2 = 1/self.r * (-1/2*vx - sqrt(3)/2*vy - self.d*w)
+            vel_3 = 1/self.r * (-1/2*vx + sqrt(3)/2*vy - self.d*w)
+
+            serial_motor(vel_1, vel_2, vel_3)
+
 
 def main(args=None):
-	rclpy.init(args=args)
+    rclpy.init(args=args)
 
-	omnino_driver_node = OmninoDriverNode()
+    omnino_driver_node = OmninoDriverNode()
 
-	rclpy.spin(omnino_driver_node)
+    rclpy.spin(omnino_driver_node)
 
-	omnino_driver_node.destroy_node()
-	rclpy.shutdown()
+    omnino_driver_node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
-	main()
-
+    main()
